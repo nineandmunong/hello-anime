c09ce23351642253fe20c42911a0ef7f08044e0a
Durapakon <durapakon@gmail.com>
2025-04-29 14:53:51 +0700

    edit_type_anime

--- src/components/Homepage/seasonalAnime/seasonalAnime.tsx
+++ src/components/Homepage/seasonalAnime/seasonalAnime.tsx
@@ -4,39 +4,77 @@ interface SeasonalAnimeProps {
   anime: SeasonalAnimeTypes[]
 }
 
+const colors: { [key: string]: string } = {
+  Action: "rgb(195, 53, 37)",
+  Adventure: "rgb(23, 146, 75)",
+  Comedy: "rgb(48, 140, 202)",
+  Fantasy: "rgb(131, 74, 153)",
+  Mystery: "rgb(226, 189, 38)",
+  Romance: "rgb(232, 139, 171)",
+  "Sci-Fi": "rgb(36, 130, 224)",
+}
+
 export default function SeasonalAnime({ anime }: SeasonalAnimeProps) {
   return (
     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 ">
       {anime.map((item, index) => (
         <div className="col d-flex" key={item.mal_id}>
           <div className="card h-100 w-100 d-flex flex-column">
-            {
-              <img
-                src={item.images.jpg.large_image_url}
-                className="card-img-top"
-                alt={item.title}
-                style={{ height: "350px", objectFit: "cover", width: "100%" }}
-              />
-            }
+            <img
+              src={item.images.jpg.large_image_url}
+              className="card-img-top"
+              alt={item.title}
+              style={{ height: "350px", objectFit: "cover", width: "100%" }}
+            />
 
-            <div className="card-body d-flex justify-content-center align-items-center">
+            <div
+              className="card-body d-flex justify-content-center align-items-center"
+              style={{ paddingBottom: "5px" }}
+            >
               <h5 className="card-title text-center"> {item.title} </h5>
             </div>
 
             <div className="d-flex flex-column mx-3 gap-1">
+              <div>
+                <h6>Rank {index + 1}</h6>
+              </div>
               <div className="d-flex justify-content-between w-100">
                 <div>
-                  <h5 className="">🌟 {item.score}</h5>
+                  <h5>🌟 {item.score}</h5>
                 </div>
                 <div>
-                  <h5 className="">👶🏻 {item.scored_by}</h5>
+                  <h5>👶🏻 {item.scored_by}</h5>
                 </div>
               </div>
 
               <div>
-                <h6 className="text-primary">
-                  {item.studios[0].name || "Unknown Studio"},{index}
+                <h6 className="text-primary my-1">
+                  🎞️ {item.studios[0].name || "Unknown Studio"}
                 </h6>
+                <div style={{ marginTop: "5px" }}>
+                  <span style={{ fontSize: "16px" }}>{item.status} * </span>
+                  <span className="text-success" style={{ fontSize: "16px" }}>
+                    {item.episodes != null
+                      ? `${item.episodes} episodes`
+                      : "? Episodes"}
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                {item.genres.map((genres) => (
+                  <span
+                    className="badge rounded-pill text-white"
+                    style={{
+                      backgroundColor: colors[genres.name] || "#7f8c8d",
+                      fontSize: "12px",
+                      margin: "10px 2px",
+                      padding: "6px 10px",
+                    }}
+                  >
+                    {genres.name}
+                  </span>
+                ))}
               </div>
             </div>
           </div>
